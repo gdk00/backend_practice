f6725e1dcd68729daa548fc9b535dca98cfc7aae
Leon Minasyan <leon-minasyan@yandex-team.ru>
2022-04-26 14:56:51 +0400

    Revert "revert: remove unis"
    This reverts commit 977fb271c80c6f8d17ab9987b0a32cfc061a7707.

--- src/sql_app/models.py
+++ src/sql_app/models.py
@@ -11,6 +11,8 @@ class Person(Base):
     password = Column(String)
     organization = relationship("Organization", back_populates="employees")
     organization_id = Column(Integer, ForeignKey("organization.id"), nullable=True)
+    university = relationship("University", back_populates="students")
+    university_id = Column(Integer, ForeignKey("university.id"), nullable=True)
 
     def __repr__(self) -> str:
         return f"User: {self.email}"
@@ -21,3 +23,10 @@ class Organization(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     employees = relationship("Person", back_populates="organization")
+
+
+class University(Base):
+    __tablename__ = "university"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True, unique=True)
+    students = relationship("Person", back_populates="university")
